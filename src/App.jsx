bdc775d11bffcfde45a3ab0b8ab96c7564ca2ab0
yash sawarkar <yashsawarkar01@gmail.com>
2024-08-31 14:57:18 +0530

    facebook authenticataion added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { LoginSocialFacebook } from "reactjs-social-login";
+import { FacebookLoginButton } from "react-social-login-buttons";
 import { useState } from "react";
 import Navbar from "./components/Navbar"
 import PageDetails from "./components/PageDetails";
@@ -9,6 +11,35 @@ function App() {
 
   return (
     <div>
+
+      <div>
+        {!profile ? (
+          <LoginSocialFacebook
+            appId="878454434176187"
+            onResolve={(response) => {
+              console.log(response);
+              setProfile(response.data);
+            }}
+            onReject={(error) => {
+              console.log(error);
+            }}
+          >
+            <FacebookLoginButton />
+          </LoginSocialFacebook>
+        ) : (
+          ""
+        )}
+
+        {profile ? (
+          <div>
+            <h1>{profile.name}</h1>
+            <img src={profile.picture.data.url} />
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+
       <Navbar handleProfile={setProfile} handleToken={setToken} />
       <UserDetails profile={profile} />
       <PageDetails accessToken={token} />
